7e33a9cb312b2c1a07f6b1dc66153c7b3ba15bf9
hananeassenal <158520265+hananeassenal@users.noreply.github.com>
2024-08-06 15:33:03 +0100

    Update news.py

--- pages/news.py
+++ pages/news.py
@@ -1,3 +1,4 @@
+import time
 import streamlit as st
 import requests
 from newspaper import Article
@@ -7,7 +8,8 @@ from pymongo import MongoClient, errors
 
 # Groq API Key
 GROQ_API_KEY = "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"
-llm = Groq(model="llama3-70b-8192", api_key=GROQ_API_KEY)
+llm = Groq(model="llama3-70b-8192", api_key=GROQ_API_KEY)  # 8 000 tokens 
+# llm = Groq(model="mixtral-8x7b-32768", api_key=GROQ_API_KEY)  # 32 000 tokens
 
 # Predefined queries by country
 queries_by_country = {
@@ -16,6 +18,13 @@ queries_by_country = {
     "Saudi": ["Saudi new data centre", "Saudi new data center"],
     "Shanghai": ["Shanghai port congestion", "Shanghai port constraint", "Shanghai port delays"]
 }
+
+# queries_by_country = {
+#     "Brazil": ["Brazil", "Brazil news", "brazil port"],
+#     "Dubai": ["Dubai", "Dubai news", "dubai port"],
+#     "Saudi": ["Saudi new data center", "Saudi"],
+#     "Shanghai": ["Shanghai port", "Shanghai news", "Shanghai"]
+# }
 
 # Function to check if user is logged in
 def check_login():
@@ -58,6 +67,11 @@ def fetch_articles(query):
 
     response = requests.post(url, json=payload, headers=headers)
 
+    if response.status_code == 429:
+            #st.warning("Too many requests. Waiting for 1 minute before retrying...")
+            time.sleep(5)
+            response = requests.post(url, json=payload, headers=headers)
+
     if response.status_code == 200:
         json_data = response.json()
         if 'news' in json_data and json_data['news']:
@@ -85,8 +99,8 @@ def fetch_articles(query):
                     st.write("---")
         else:
             st.error("No articles found.")
-    else:
-        st.error(f"API request error: {response.status_code} - {response.reason}")
+    # else:
+    #     st.error(f"API request error: {response.status_code} - {response.reason}")
 
 def display_article(article):
     st.markdown(f"""
@@ -122,10 +136,45 @@ def save_article(article):
         upsert=True
     )
 
+# def main():
+#     check_login()  # Ensure the user is logged in
+
+#     st.header(f"News Articles")
+    
+#     # Ensure country is set from session state
+#     if 'country' not in st.session_state:
+#         st.session_state.country = "Brazil"  # Default country if not set
+
+#     # Get list of countries
+#     countries = ["Brazil", "Dubai", "Saudi", "Shanghai"]
+
+#     # Ensure the session state country is valid
+#     if st.session_state.country not in countries:
+#         st.session_state.country = "Brazil"
+
+#     # Get the index of the default country
+#     default_index = countries.index(st.session_state.country)
+
+#     country = st.selectbox("Select Country", countries, index=default_index)
+#     st.session_state.country = country
+
+#     st.subheader("Search News")
+#     query = st.text_input("Enter search query")
+
+#     if query:
+#         fetch_articles(query)
+#     else:
+#         queries = queries_by_country.get(st.session_state.country, [])
+#         for query in queries:
+#             fetch_articles(query)
+
+# if __name__ == "__main__":
+#     main()
+
 def main():
     check_login()  # Ensure the user is logged in
 
-    st.header(f"News Articles")
+    st.header("News Articles")
     
     # Ensure country is set from session state
     if 'country' not in st.session_state:
@@ -141,8 +190,13 @@ def main():
     # Get the index of the default country
     default_index = countries.index(st.session_state.country)
 
-    country = st.selectbox("Select Country", countries, index=default_index)
-    st.session_state.country = country
+    # Select country and update session state
+    country = st.selectbox("Select Country", countries, index=default_index, key='country_select')
+
+    # Check if the selected country is different from the session state
+    if country != st.session_state.country:
+        st.session_state.country = country
+        st.rerun()  # Trigger rerun if country is changed
 
     st.subheader("Search News")
     query = st.text_input("Enter search query")
@@ -156,3 +210,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
